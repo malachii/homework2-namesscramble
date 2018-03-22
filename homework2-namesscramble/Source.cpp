016e1b00f69a6d923688bf14c52d5a92c9232336
Matthew <seizureboy@gmail.com>
2018-03-22 14:06:10 -0400

    No loop, cheated with random_shuffle

---
+++ homework2-namesscramble/Source.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str1;
+	cout << "First Name? ";
+	cin >> str1;
+	string str2;
+	cout << "Last name? ";
+	cin >> str2;
+	random_shuffle(str1.begin(), str1.end());
+	random_shuffle(str2.begin(), str2.end());
+	cout << str1 << '\n';
+	cout << str2 << '\n';
+	cout << "Your name should be confused";
+	return 0;
+
+}
+	
