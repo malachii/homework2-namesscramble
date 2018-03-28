ef4548f19860a2de01ae1da42708b9dfeaad4ca2
Matthew <seizureboy@gmail.com>
2018-03-28 09:26:07 -0400

    works but I know I didnt need two for loops

--- homework2-namesscramble/Source.cpp
+++ homework2-namesscramble/Source.cpp
@@ -3,20 +3,25 @@
 #include <string>
 using namespace std;
 
-int main()
-{
+int main(){
 	string str1;
 	cout << "First Name? ";
 	cin >> str1;
 	string str2;
-	cout << "Last name? ";
+	cout << "Last Name? ";
 	cin >> str2;
-	random_shuffle(str1.begin(), str1.end());
-	random_shuffle(str2.begin(), str2.end());
-	cout << str1 << "" << str2  << '\n';
-	//cout << str2 << '\n';
-	cout << "Your name should be confused";
-	return 0;
+	string reverse (str1);
+	for (int i = str1.length(); i >= 0; --i)
+	{
+		cout << str1[i];
+	}
 
-}
+	for (int b = str2.length(); b >= 0; --b)
+	{
+		cout << str2[b];
+	}
 	
+	return 0;
+		
+
+	}
